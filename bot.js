db82f5931cc983ae9c21cc255d58454b2c9a640d
yosefsss <53384829+yosefsss@users.noreply.github.com>
2019-07-29 04:34:20 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("605140105048489986")
+setInterval(function() {
+channel.send("credits credits credits credits credits credits credits credits credits credits credits credits credits credits credits credits");
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
